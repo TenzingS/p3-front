be6e98855d9bc5ffbb81940fade1d3151ce29a01
Prince <avecillasprince@yahoo.com>
2021-08-26 23:55:21 -0400

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,87 +19,86 @@ function Home() {
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.coindesk.com/markets/2021/08/26/market-wrap-crypto-pullback-deepens-expect-higher-volatility/">
                         <img 
                             className="image"
-                            src="https://dimg.donga.com/wps/NEWS/IMAGE/2021/08/17/108583297.2.gif" 
+                            src="https://stevesammartino.com/wp-content/uploads/2021/03/bitcoin-gif.gif" 
                             alt="news" width="600" height="400" />
                         </a>
-                        <h1 className="newstitle">Taliban Take Over Afghanistan.</h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <h1 className="newstitle">Market Wrap: Crypto Pullback Deepens; Expect Higher Volatility</h1>
+                        <p className = 'description'>Cryptocurrencies were mostly lower on Thursday as bullish sentiment wanes. 
+                        Stocks were also lower as Federal Reserve Bank of Dallas President Robert Kaplan mentioned during an interview on CNBC that he would like to see tapering of bond purchases announced in September. 
+                        Bitcoin was trading around $46,000 at press time and is down about 4% over the past 24 hours. Technical charts suggest the pullback could stabilize between the $42,000-$45,000 support zone.</p>
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.cnn.com/2021/08/25/tech/onlyfans-suspends-ban/index.html">
                         <img 
                             className="image"
                             src="https://c.tenor.com/cb6WtBTLxooAAAAC/onlyfans-onlyfans-creators.gif" 
                             alt="news" width="600" height="400" />
                         </a>
                         <h1 className="newstitle">Thoughts rejoice as OnlyFans suspends policy change </h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <p className = 'description'>OnlyFans said Wednesday it has “suspended” plans to ban pornography, in a stunning U-turn that came after fierce backlash from thoughs and simpsons. 
+                        A spokesperson for the online subscription platform told CNBC that the proposed changes were no longer required “due to banking partners’ assurances that OnlyFans can support all genres of creators.” 
+                        The London-based firm last week announced plans to prohibit porn from Oct. 1, citing pressure from its banking partners. 
+                        The news has allowed content creators to continue their fatherless endeavours</p>
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.usnews.com/news/business/articles/2021-08-26/asian-stocks-slip-following-record-wall-street-charge">
                         <img 
                             className="image"
-                            src="https://dimg.donga.com/wps/NEWS/IMAGE/2021/08/17/108583297.2.gif" 
+                            src="https://www.optimizedtrading.com/uploads/7/9/8/4/79848180/3453777_orig.gif" 
                             alt="news" width="600" height="400" />
                         </a>
-                        <h1 className="newstitle">Taliban Take Over Afghanistan.</h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <h1 className="newstitle">Stocks Fall After Kabul Bombing; Traders Also Wait for Fed</h1>
+                        <p className = 'description'>Technology and communication companies led a broad sell-off on Wall Street Thursday following deadly suicide attacks at the Kabul airport in Afghanistan. 
+                    The S&P 500 fell 0.6% a day after capping a five-day winning streak with an all-time high. 
+                    The Dow Jones Industrial Average fell 0.5%, while the Nasdaq composite lost 0.6%. Despite the losses, the three major indexes are on track for weekly gains. 
+                    Twin suicide bombings struck Thursday outside Kabul’s airport, where large crowds of people trying to flee Afghanistan have massed. 
+                    </p>
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.cnbc.com/2021/08/26/japan-pulls-1point6-million-moderna-vaccine-doses-over-contamination-concerns-.html">
                         <img 
                             className="image"
-                            src="https://dimg.donga.com/wps/NEWS/IMAGE/2021/08/17/108583297.2.gif" 
+                            src="https://ewscripps.brightspotcdn.com/dims4/default/7e33fab/2147483647/strip/true/crop/800x450+0+0/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2Ff1%2F5c%2F6c81ad5d4ef5b8ecc42517e2c707%2Fcovidvaccinegif.gif" 
                             alt="news" width="600" height="400" />
                         </a>
-                        <h1 className="newstitle">Taliban Take Over Afghanistan.</h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <h1 className="newstitle">Japan pulls 1.6 million Moderna vaccine doses over contamination concerns.</h1>
+                        <p className = 'description'>Japan’s Ministry of Health, Labour and Welfare suspended the use of more than 1.6 million doses of Moderna’s Covid-19 vaccine Thursday following reports of contaminated vials at multiple vaccination sites there. 
+                    Takeda Pharmaceuticals, which distributes the vaccine in Japan for Moderna, said it notified the ministry after several vaccination sites reported an unspecified foreign object found in one specific lot. 
+                    “We requested that Moderna...</p>
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.pcmag.com/news/report-major-chip-maker-tsmc-to-raise-prices">
                         <img 
                             className="image"
-                            src="https://dimg.donga.com/wps/NEWS/IMAGE/2021/08/17/108583297.2.gif" 
+                            src="https://ucarecdn.com/f15cf123-b2c8-4f29-8586-1b25ecaedac7/BMS%E5%8A%A8%E5%9B%BE-GIF-1.gif" 
                             alt="news" width="600" height="400" />
                         </a>
-                        <h1 className="newstitle">Taliban Take Over Afghanistan</h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <h1 className="newstitle">Report: Major Chip Maker TSMC to Raise Prices</h1>
+                        <p className = 'description'>CPU processor, laptop, and smartphone costs could be on the rise thanks to price hikes from the world’s largest chip manufacturers. 
+                        Semiconductor foundry TSMC plans on hiking up its prices by as much as 20% later this year or by early next year, according to The Wall Street Journal, citing unnamed sources. 
+                        The foundry counts Apple, AMD, and Qualcomm among its many clients. Most notably, TSMC’s 7-nanometer chip manufacturing process is behind AMD’s Ryzen 5000 CPU processors and the graphics technology inside the PlayStation 5 and Xbox Series X. 
+                        </p>
                     </div>
                     <div className="individualarticle">
                         <a className="click" target="_blank" rel="noopener noreferrer"
-                            href= "https://www.jpost.com/middle-east/taliban-hijacks-theme-park-in-afghanistan-watch-677015">
+                            href= "https://www.cnbc.com/2021/08/26/education-department-will-cancel-student-debt-for-115000-borrowers-.html">
                         <img 
                             className="image"
-                            src="https://dimg.donga.com/wps/NEWS/IMAGE/2021/08/17/108583297.2.gif" 
+                            src="https://media4.giphy.com/media/jSJUgf5Ibd4Q4sAlaQ/giphy.gif?cid=790b76111b2e4de86314dac0d5c7756cf509c1abe9a4ae03&rid=giphy.gif&ct=g" 
                             alt="news" width="600" height="400" />
                         </a>
-                        <h1 className="newstitle">Taliban Take Over Afghanistan.</h1>
-                        <p className = 'description'>A theme park in Afghanistan was hijacked by the Taliban, with photos and videos of the militant group riding the park's attractions going viral on social media, N12 reported on Tuesday. 
-                            Videos uploaded to Twitter earlier this week of the militant group received as many as 75,000 likes as of Wednesday morning. 
-                            Between the conflicts of the streets in the capital Kabul, the Taliban fighters also found time to work out in the gym in the presidential headquarters and play sports. 
-                            Others jumped on the trampoline and swayed on swings.</p>
+                        <h1 className="newstitle">Education Department will cancel student debt for 115,000 borrowers</h1>
+                        <p className = 'description'>The U.S. Department of Education announced Thursday it will cancel $1.1 billion in student debt for 115,000 borrowers. 
+                            The relief will go to those who attended ITT Technical Institute, a for-profit school that is now defunct. 
+                            The school is accused of misrepresenting itself and steering students into pricey private loans. These borrowers didn’t finish their degrees and left the school after March 31, 2008. 
+                            “Today’s action continues the Department’s efforts to improve and use its targeted loan relief authorities to deliver meaningful help to student borrowers,” U.S. Secretary of Education...</p>
                     </div>
             </div>                        
         </div>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -22,7 +22,7 @@ function Stock({stock, onBuyStocks, user, onHandleFunds, handleRandomStock}) {
               })
               .then((r) => r.json())
               .then((stockz) => handleRandomStock(stockz));
-      }, 3000);
+      }, 1000);
       return () => clearInterval(interval);
   }, [])
 
